a3c0dd39a8f67a5e64264792dce25b925cfe8e3f
giovannigamarra <giovanni_268018@hotmail.com>
2021-04-24 10:50:47 -0500

    commit

--- TiendaRopa/Controllers/BusquedaController.cs
+++ TiendaRopa/Controllers/BusquedaController.cs
@@ -2,6 +2,26 @@ namespace TiendaRopa.Controllers
 {
     public class BusquedaController
     {
-        
-    }
+      public BusquedaController(ILogger<BusquedaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }  
+    } 
+   
 }
