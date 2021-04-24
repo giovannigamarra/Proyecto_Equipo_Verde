e8f9df0af0152fa33fec894a043b5c2b1aef5f42
JhonFrankYdme <jhonfrankdymebenito@gmail.com>
2021-04-24 10:40:43 -0500

    Ventas

---
+++ TiendaRopa/Controllers/ContactoController.cs
@@ -0,0 +1,7 @@
+namespace TiendaRopa.Controllers
+{
+    public class ContactoController
+    {
+        
+    }
+}
---
+++ TiendaRopa/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TiendaRopa.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
