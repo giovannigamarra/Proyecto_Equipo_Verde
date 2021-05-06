1dbee96a0aa6afbd8d66f7b4dfad400ea698a09e
giovannigamarra <giovanni_268018@hotmail.com>
2021-05-06 18:11:48 -0500

    admin

---
+++ TiendaRopa/Controllers/ConocenosController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TiendaRopa.Models;
+namespace TiendaRopa.Controllers
+{ 
+    public class ConocenosController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ConocenosController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
